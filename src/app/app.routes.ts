ef129232457e97240065688272bfa87f2c4e16db
JanxJan12 <jcenia24467@liceo.edu.ph>
2026-01-07 13:23:18 +0800

    Prelim Exam

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PrelimExamComponent } from "./prelim-exam/prelim-exam.component";
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+  path: 'prelim-exam',
+  component: PrelimExamComponent
+}
+
+];
